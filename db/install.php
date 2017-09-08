d6d563ed74a73c5bca4efa79a5d77eac564c22f5
David Monllao <david.monllao@gmail.com>
2017-09-08 14:56:19 +0200

    Check if present before creating models

--- db/install.php
+++ db/install.php
@@ -18,34 +18,54 @@
 defined('MOODLE_INTERNAL') || die();
 
 function xmldb_local_testanalytics_install() {
+    global $DB;
 
     \core\session\manager::set_user(get_admin());
+
+    $usedtargets = $DB->get_fieldset_select('analytics_models', 'DISTINCT target', '');
 
     $indicator = \core_analytics\manager::get_indicator('\local_testanalytics\analytics\indicator\set_setting');
     $indicators = array($indicator->get_id() => $indicator);
 
-    $target = \core_analytics\manager::get_target('\local_testanalytics\analytics\target\linear_example');
-    $model = \core_analytics\model::create($target, $indicators, '\core\analytics\time_splitting\single_range');
-    $model->enable();
+    if (!in_array('\local_testanalytics\analytics\target\linear_example', $usedtargets)) {
+        $target = \core_analytics\manager::get_target('\local_testanalytics\analytics\target\linear_example');
+        $model = \core_analytics\model::create($target, $indicators, '\core\analytics\time_splitting\single_range');
+        $model->enable();
+    }
 
+    if (!in_array('\local_testanalytics\analytics\target\discrete_example', $usedtargets)) {
+        $target = \core_analytics\manager::get_target('\local_testanalytics\analytics\target\discrete_example');
+        $model = \core_analytics\model::create($target, $indicators, '\core\analytics\time_splitting\single_range');
+        $model->enable();
+    }
 
-    $target = \core_analytics\manager::get_target('\local_testanalytics\analytics\target\discrete_example');
-    $model = \core_analytics\model::create($target, $indicators, '\core\analytics\time_splitting\single_range');
-    $model->enable();
+    if (!in_array('\local_testanalytics\analytics\target\binary_example', $usedtargets)) {
+        $target = \core_analytics\manager::get_target('\local_testanalytics\analytics\target\binary_example');
+        $model = \core_analytics\model::create($target, $indicators, '\core\analytics\time_splitting\single_range');
+        $model->enable();
+    }
 
-    $target = \core_analytics\manager::get_target('\local_testanalytics\analytics\target\binary_example');
-    $model = \core_analytics\model::create($target, $indicators, '\core\analytics\time_splitting\single_range');
-    $model->enable();
+    if (!in_array('\local_testanalytics\analytics\target\undead_users', $usedtargets)) {
+        $indicator = \core_analytics\manager::get_indicator('\local_testanalytics\analytics\indicator\user_suspended');
+        $indicators = array($indicator->get_id() => $indicator);
+        $target = \core_analytics\manager::get_target('\local_testanalytics\analytics\target\undead_users');
+        $model = \core_analytics\model::create($target, $indicators, '\core\analytics\time_splitting\single_range');
+        $model->enable();
+    }
 
-    $indicator = \core_analytics\manager::get_indicator('\local_testanalytics\analytics\indicator\user_suspended');
-    $indicators = array($indicator->get_id() => $indicator);
-    $target = \core_analytics\manager::get_target('\local_testanalytics\analytics\target\undead_users');
-    $model = \core_analytics\model::create($target, $indicators, '\core\analytics\time_splitting\single_range');
-    $model->enable();
+    if (!in_array('\local_testanalytics\analytics\target\useless_categories', $usedtargets)) {
+        $indicator = \core_analytics\manager::get_indicator('\local_testanalytics\analytics\indicator\category_empty');
+        $indicators = array($indicator->get_id() => $indicator);
+        $target = \core_analytics\manager::get_target('\local_testanalytics\analytics\target\useless_categories');
+        $model = \core_analytics\model::create($target, $indicators, '\core\analytics\time_splitting\single_range');
+        $model->enable();
+    }
 
-    $indicator = \core_analytics\manager::get_indicator('\local_testanalytics\analytics\indicator\category_empty');
-    $indicators = array($indicator->get_id() => $indicator);
-    $target = \core_analytics\manager::get_target('\local_testanalytics\analytics\target\useless_categories');
-    $model = \core_analytics\model::create($target, $indicators, '\core\analytics\time_splitting\single_range');
-    $model->enable();
+    if (!in_array('\local_testanalytics\analytics\target\without_picture', $usedtargets)) {
+        $indicator = \core_analytics\manager::get_indicator('\local_testanalytics\analytics\indicator\user_suspended');
+        $indicators = array($indicator->get_id() => $indicator);
+        $target = \core_analytics\manager::get_target('\local_testanalytics\analytics\target\without_picture');
+        $model = \core_analytics\model::create($target, $indicators, '\core\analytics\time_splitting\single_range');
+        $model->enable();
+    }
 }
